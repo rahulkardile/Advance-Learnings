8e4160848b399c3f3c4d7f51e6808f189616c4cd
Rahul kardile <rahulkardile321@gmail.com>
2024-08-06 11:46:32 +0530

    new query added

--- GraphQL/server/index.js
+++ GraphQL/server/index.js
@@ -9,24 +9,39 @@ async function startServer() {
     const app = express();
     const server = new ApolloServer({
         typeDefs: `
+        type User {
+            id: ID!
+            name: String!
+            username: String!
+            email: String!
+            phone: String!
+            website: String!
+        }
+
         type Todo {
             id: ID!
             title: String!
             completed: Boolean
+            userId: ID!
         },
         
         type Query {
             getTodos: [Todo]
+            getAllUsers: [User]
+            getUser(id: ID!): User
         }
         
         `,
         resolvers: {
             Query: {
-                getTodos: async()=>  (await axios.get("https://jsonplaceholder.typicode.com/todos")).data }
+                getTodos: async()=>  (await axios.get("https://jsonplaceholder.typicode.com/todos")).data,
+                getAllUsers: async()=>  (await axios.get("https://jsonplaceholder.typicode.com/users")).data, 
+                getUser: async(parent, { id })=>  (await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)).data, 
+            }
         },
     });
 
-    app.use(bodyParser.json())
+    app.use(bodyParser.json());
     app.use(cors());
 
     await server.start();
