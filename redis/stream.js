b26b8d4c2ff5e34b255a1bde1a6da320aeef94e5
Rahul kardile <rahulkardile321@gmail.com>
2024-09-01 09:03:13 +0530

    stream is updated

--- redis/stream.js
+++ redis/stream.js
@@ -2,38 +2,39 @@ import { redisClient } from "./client.js";
 
 async function init() {
 
-    await redisClient.xadd("race:france", '*', 
-        'rider', 'Castilla',
-        'speed', '30.2',
-        'position', '1',
-        'location_id', '1'
-    );
+    // await redisClient.xadd("race:france", '*', 
+    //     'rider', 'Castilla',
+    //     'speed', '30.2',
+    //     'position', '1',
+    //     'location_id', '1'
+    // );
 
-    await redisClient.xadd("race:france", '*', 
-        'rider', 'raju sharma',
-        'speed', '32.2',
-        'position', '3',
-        'location_id', '1'
-    );
+    // await redisClient.xadd("race:france", '*', 
+    //     'rider', 'raju sharma',
+    //     'speed', '32.2',
+    //     'position', '3',
+    //     'location_id', '1'
+    // );
 
-    await redisClient.xadd("race:france", '*', 
-        'rider', 'nue',
-        'speed', '37.2',
-        'position', '2',
-        'location_id', '1'
-    );
+    // await redisClient.xadd("race:france", '*', 
+    //     'rider', 'nue',
+    //     'speed', '37.2',
+    //     'position', '2',
+    //     'location_id', '1'
+    // );
 
-    await redisClient.xadd("race:france", '*', 
-        'rider', 'jain',
-        'speed', '21.2',
-        'position', '4',
-        'location_id', '1'
-    );
+    // await redisClient.xadd("race:france", '*', 
+    //     'rider', 'jain',
+    //     'speed', '21.2',
+    //     'position', '4',
+    //     'location_id', '1'
+    // );
 
-    console.log(await redisClient.xrange('race:france', '0', "+"));
+    // Fetch all entries from the stream
+    // console.log(await redisClient.xrange('race:france', '0', "+"));
 
-    // const res4 = await client.xRange('race:france', '1691765278160-0', '+', {COUNT: 2});
-    // console.log(res4); 
+    // Fetch two entries starting from a specific ID using COUNT option
+    console.log(await redisClient.xrange('race:france', '1725161058342-0', '+', 'COUNT', 4)); 
 
 }
 
