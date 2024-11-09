a176555115db78856c3dcc65476c32be50087966
Rahul kardile <rahulkardile321@gmail.com>
2024-11-09 23:59:46 +0530

    basic dns server setup

--- my-dns/src/index.ts
+++ my-dns/src/index.ts
@@ -1,4 +1,12 @@
 import express, { Request, Response } from 'express';
+import dgram from "node:dgram";
+
+const server = dgram.createSocket("udp4");
+
+server.on('message', (msg, rinfo)=> {
+    
+})
+
 
 const app = express();
 const port = 3000;
