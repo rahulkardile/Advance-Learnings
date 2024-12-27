1951d813ee22353c27830cf2f8c02ee1fa0c17a5
rahulkardile <rahulkardile321@gmail.com>
2024-12-27 14:34:56 +0530

    explored basic global in node js

---
+++ node core/global/app.js
@@ -0,0 +1,10 @@
+// console.log("Hello World");
+
+global.console.log("This from Global");
+
+// global is working as global but it you use this global in browser then you get window object.
+// console.log(globalThis);
+
+globalThis.console.log("This from GlobalThis");
+
+
