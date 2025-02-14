43f4f5f365a35fbb53c844374d80675a98a6e4ab
rahulkardile <rahulkardile321@gmail.com>
2025-02-14 07:36:02 +0530

    first pattern

---
+++ DSA/patterns/Pattern.java
@@ -0,0 +1,22 @@
+package patterns;
+
+public class Pattern {
+
+    // *****
+    // *****
+    // *****
+    // *****
+    public static void Pattern1() {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Pattern1();
+    }
+
+}
