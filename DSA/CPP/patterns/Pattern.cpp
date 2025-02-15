520a2c16cc9b5725365c1c43b15cd2f9139a177c
rahulkardile <rkardile@lentra.ai>
2025-02-15 05:53:01 +0530

    2 patterns added

--- DSA/CPP/patterns/Pattern.cpp
+++ DSA/CPP/patterns/Pattern.cpp
@@ -1,15 +1,51 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void print1(){
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            cout << "*" << endl;
+// * * * * 
+// * * * * 
+// * * * * 
+// * * * * 
+void print1() {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            cout << "* "; 
         }
         cout << endl;
     }
 }
 
-int main(){
-    print1();
+// *
+// **
+// ***
+// ****
+// *****
+void print2(){
+    for (int i = 0; i <= 5; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;        
+    }
+    
+}
+
+void print3() {
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+
+
+int main() {
+    //  compile it throw cmd : g++ CPP/patterns/Pattern.cpp  -o program 
+    //  run ./program 
+
+    print3();
+    return 0;
+
 }
