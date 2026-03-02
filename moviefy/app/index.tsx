b3fd8f7b7d0b6394e03e782a4224d1709cf1af4b
Rahul Kardile <rahulkardile321@gmail.com>
2026-03-03 05:12:14 +0530

    new app added

---
+++ moviefy/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center bg-blue-50">
+      <Text className="text-2xl font-bold text-blue-800">Welcome to Moviefy!</Text>
+    </View>
+  );
+}
